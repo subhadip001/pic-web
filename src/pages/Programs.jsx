2491c9e6313f5afe0987df01155049197c2a35ad
deepak <bairwaddeepak@gmail.com>
2023-06-08 13:48:15 +0530

    placement procedure

---
+++ src/pages/Programs.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Programs = () => {
+    const [activeStep, setActiveStep] = useState();
+  const steps = [1, 2, 3];
+  return (
+    <div>Programs</div>
+  )
+}
+
+export default Programs
