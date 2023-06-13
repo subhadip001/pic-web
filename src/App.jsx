506bd5d15843cd55d2ff6ddf9dc52de81ce9b9a9
Subhadip <mnhacker2001@gmail.com>
2023-06-13 13:45:31 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import Home from './pages/Home.jsx'
-import Header from './components/Header'
-import Footer from './components/Footer.jsx'
-import { Route, Routes } from 'react-router-dom'
-import Messagefull from './components/Messagefull.jsx'
-import Administration from './pages/Administration.jsx'
-import FormComponent from './components/FormComponent.jsx'
-import PlacementProcedure from './pages/PlacementProcedure.jsx'
-import Programs from './pages/Programs.jsx'
-import Departments from './pages/Departments.jsx'
-import QuickLink from './components/QuickLink.jsx'
-import Recuiters from './pages/Recuiters.jsx'
-import Infrastructure from './pages/Infrastructure.jsx'
-import GuestHouse from './pages/GuestHouse.jsx'
-import Hotels from './pages/Hotels.jsx'
-import ContactDetails from './pages/ContactDetails.jsx'
-import Faq from './pages/Faq.jsx'
-import HowToReach from './pages/HowToReach.jsx'
+import React from "react";
+import Home from "./pages/Home.jsx";
+import Header from "./components/Header";
+import Footer from "./components/Footer.jsx";
+import { Route, Routes } from "react-router-dom";
+import Messagefull from "./components/Messagefull.jsx";
+import Administration from "./pages/Administration.jsx";
+import FormComponent from "./components/FormComponent.jsx";
+import PlacementProcedure from "./pages/PlacementProcedure.jsx";
+import Programs from "./pages/Programs.jsx";
+import Departments from "./pages/Departments.jsx";
+import QuickLink from "./components/QuickLink.jsx";
+import Recuiters from "./pages/Recuiters.jsx";
+import Infrastructure from "./pages/Infrastructure.jsx";
+import GuestHouse from "./pages/GuestHouse.jsx";
+import Hotels from "./pages/Hotels.jsx";
+import ContactDetails from "./pages/ContactDetails.jsx";
+import Faq from "./pages/Faq.jsx";
+import HowToReach from "./pages/HowToReach.jsx";
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path="/message/:by" element={<Messagefull/>}/>
-      <Route path="/administration" element={<Administration/>}/>
-      <Route path="/forms/:id" element={<FormComponent/>}/>
-      <Route path='/placement' element={<PlacementProcedure/>}/>
-      <Route path='/programs' element={<Programs/>}/>
-      <Route path='/departments' element={<Departments/>}/>
-      <Route path='/recruiters' element={<Recuiters/>}/>
-      <Route path='/Infrastructure' element={<Infrastructure/>}/>
-      <Route path='/guest-house' element={<GuestHouse/>}/>
-      <Route path='/hotels' element={<Hotels/>}/>
-      <Route path='/contact-details' element={<ContactDetails/>}/>
-      <Route path='/faq' element={<Faq/>}/>
-      <Route path='/how-to-reach' element={<HowToReach/>}/>
-    </Routes>
-   <QuickLink/>
-   <Footer/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/message/:by" element={<Messagefull />} />
+        <Route path="/administration" element={<Administration />} />
+        <Route path="/forms/:id" element={<FormComponent />} />
+        <Route path="/placement" element={<PlacementProcedure />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/departments" element={<Departments />} />
+        <Route path="/recruiters" element={<Recuiters />} />
+        <Route path="/Infrastructure" element={<Infrastructure />} />
+        <Route path="/guest-house" element={<GuestHouse />} />
+        <Route path="/hotels" element={<Hotels />} />
+        <Route path="/contact-details" element={<ContactDetails />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/how-to-reach" element={<HowToReach />} />
+      </Routes>
+      <QuickLink />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,57 @@
-import React from 'react'
-import '../styles/footer.css'
-import logo from '../assets/logo.png'
+import React from "react";
+import "../styles/footer.css";
+import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 const Footer = () => {
-    return (
-        <>
-            <div className='footer'>
-                <div className='footer-item' >
-                    <img className='logo' src={logo} alt="logo" />
-                    <div className='disc'>
-                        <p>Placement and Internship Cell,</p>
-                        <p>Indian Institute of Technology Roorkee,</p>
-                        <p>Roorkee-247667</p>
-                        <p>Distt. Haridwar, Uttarakhand, India</p>
-                    </div>
-                </div>
-                <div className='footer-item'>
-                    <h2>Contact Us</h2>
-                    <div className='disc'>
-                        <h3>Phone</h3>
-                        <a className='footer-phone' href="tel:+91-1332-285260">+91 - 1332 - 285260</a>
-                        <h3>Email</h3>
-                        <a className='footer-email' href="mailTo:placement@iitr.ac.in">placement@iitr.ac.in or pic@iitr.ac.in</a>
-                    </div>
+  return (
+    <>
+      <div className="footer">
+        <div className="footer-item">
+          <img className="logo" src={logo} alt="logo" />
+          <div className="disc">
+            <p>Placement and Internship Cell,</p>
+            <p>Indian Institute of Technology Roorkee,</p>
+            <p>Roorkee-247667</p>
+            <p>Distt. Haridwar, Uttarakhand, India</p>
+          </div>
+        </div>
+        <div className="footer-item">
+          <h2>Contact Us</h2>
+          <div className="disc">
+            <h3>Phone</h3>
+            <a className="footer-phone" href="tel:+91-1332-285260">
+              +91 - 1332 - 285260
+            </a>
+            <h3>Email</h3>
+            <a className="footer-email" href="mailTo:placement@iitr.ac.in">
+              placement@iitr.ac.in or pic@iitr.ac.in
+            </a>
+          </div>
+        </div>
+        <div className="footer-item">
+          <h2>Direct Links</h2>
+          <div className="disc">
+            <NavLink to="/placement">Placement Procedure</NavLink>
+            <NavLink href="#">E-Brochures</NavLink>
+            <NavLink to="/forms/job-announcement">Forms</NavLink>
+            <NavLink to="/faq#">FAQs</NavLink>
+          </div>
+        </div>
 
-                </div>
-                <div className='footer-item'>
-                    <h2>Direct Links</h2>
-                    <div className='disc'>
-                        <a href="#">Placement Procedure</a>
-                        <a href="#">E-Brochures</a>
-                        <a href="#">Forms</a>
-                        <a href="#">FAQs</a>
-                    </div>
+        <hr />
+        <div className="copy-right">
+          <p className="copy-right-p">
+            Copyright © 2023 Placement and Internship Cell, IITR. All rights
+            reserved.
+          </p>
+          <p>
+            Designed by Atharva Duraphe, Kavin Madheswaran, S. Rabiudeen and
+            Sheetal Narkhede | Developed by Cypher Tech
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                </div>
-
-                <hr />
-                <div className='copy-right'>
-                    <p className='copy-right-p'>Copyright © 2023 Placement and Internship Cell, IITR. All rights reserved.</p>
-                    <p>Designed by Atharva Duraphe, Kavin Madheswaran, S. Rabiudeen and Sheetal Narkhede    |    Developed by Cypher Tech</p>
-                </div>
-            </div>
-        </>
-
-    )
-}
-
-export default Footer
+export default Footer;
