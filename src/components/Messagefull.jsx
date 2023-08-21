528537e31da28c4fe5a9cfaea62b2d407a0bda17
deepak bairwa <bairwaddeepak@gmail.com>
2023-08-21 22:08:37 +0530

    ..

--- src/components/Messagefull.jsx
+++ src/components/Messagefull.jsx
@@ -12,17 +12,17 @@ const Messagefull = () => {
       <Header bgOn={true} />
       <div className=" m-auto w-[1144px] 3xl:w-[1430px] text-lg red_hat_text">
         <h3 className="heading  ">{data[0].heading}</h3>
-        <div className="flex flex-wrap mb-14 pb-[32px] 3xl:pb-[40px] rounded-md  mt-14 3xl:mt-[72px] " style={{ background: "#F4F8F6" }}>
+        <div className="flex flex-wrap mb-14 pb-[32px] 3xl:pb-[40px] rounded-xl  mt-14 3xl:mt-[72px] " style={{ background: "#F4F8F6" }}>
           <div className="flex flex-col sm:flex-row ">
-            <div className="w-[446px] object-center rounded-md">
+            <div className="w-[446px] object-center rounded-xl">
               <img
-                className=" object-contain w-[440px] rounded-md max-w-[440px]"
+                className=" object-contain w-[440px] rounded-xl max-w-[440px]"
                 height={"410px"}
                 src={data[0].img}
                 alt=""
               />
             </div>
-            <div className=" flex flex-col">
+            <div className=" flex flex-col  ">
               <div className=" font-bold bg-white pt-[40px] pl-14 pb-[19px] 3xl:pb-[24px]">
                 <h4 className="message-by mb-0 text-[22px] 3xl:text-[28px] font-bold red_hat_text">
                   {data[0].by}
@@ -34,7 +34,7 @@ const Messagefull = () => {
                   {data[0].institute}
                 </p>
               </div>
-              <div className=" px-14 text-justify pt-8">
+              <div className=" px-14 text-justify pt-8 ">
                 {data[0].greeting && (
                   <p className="greeting block mb-1 text-[18px] 3xl:text-[22px] pb-[18px] 3xl:pb-[26px]">
                     {data[0].greeting}{" "}
